2c81dd22c9a96fda22f482227e9b46d354f5e20e
Ashwani <kumar.ashwani@live.in>
2022-07-20 09:39:40 +0530

    added remaining code for control value accessor

--- projects/input-lib/src/lib/input-lib.component.ts
+++ projects/input-lib/src/lib/input-lib.component.ts
@@ -1,37 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-import { ControlValueAccessor, FormControl, FormGroup } from '@angular/forms';
+import { Component, forwardRef, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  FormControl,
+  FormGroup,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  ValidationErrors,
+  Validator,
+} from '@angular/forms';
 
 @Component({
   selector: 'lib-input',
   templateUrl: './input-lib.component.html',
   styleUrls: ['./input-lib.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputLibComponent),
+      multi: true,
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => InputLibComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputLibComponent implements OnInit, ControlValueAccessor {
-  form: FormGroup;
+export class InputLibComponent
+  implements OnInit, ControlValueAccessor, Validator
+{
+  formControl = new FormControl();
 
-  constructor() {
-    this.form = new FormGroup({
-      input: new FormControl(),
-    });
-  }
+  public onTouched: () => void = () => {};
+
+  constructor() {}
 
   ngOnInit(): void {}
 
   writeValue(obj: any): void {
-    console.log(obj);
-
-    this.form.setValue({
-      input: obj,
-    });
+    if (obj) {
+      this.formControl.setValue(obj);
+    }
   }
 
   registerOnChange(fn: any): void {
-    // throw new Error('Method not implemented.');
-
-    console.log(fn);
+    this.formControl.valueChanges.subscribe(fn);
   }
 
   registerOnTouched(fn: any): void {
-    // throw new Error('Method not implemented.');
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    isDisabled ? this.formControl.disable() : this.formControl.enable();
+  }
+
+  validate(c: AbstractControl): ValidationErrors | null {
+    //console.log("AreaSearchComponent validate", c, !this.areaForm.invalid);
+    return !this.formControl.invalid
+      ? null
+      : { invalidForm: { valid: false, message: 'invalid' } };
   }
 }
